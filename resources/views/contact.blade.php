25e6c73bb215a5676fbcac12d8d57c3bd8efd513
Angela <antunezangela89@gmail.com>
2024-02-19 08:46:06 +0100

    Add sections to basic views

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-</head>
-<body>
+@extends('layouts.landing')
+
+
+@section('title', 'Contact')
+
+@section('content')
     <h1>Contact</h1>
-</body>
-</html>
+@endsection
