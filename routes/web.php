7ba74e29f9886c43401d32b0fdadd38c41850a3a
Angela <antunezangela89@gmail.com>
2024-02-19 07:49:50 +0100

    Clean up

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'landing.index')->name('index');
 Route::view('/about', 'landing.about')->name('about');
-Route::view('/welcome', 'welcome')->name('welcome');
